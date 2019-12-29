238050cf3a58ba5f355121dba9f9b7c92a31843f
Rick Sun <yatyricky@gmail.com>
2019-12-29 23:25:59 +0800

    1

---
+++ EndlessFarmUnity/Assets/Scripts/Lang/Field.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace WE
+{
+    public enum Career
+    {
+        TANK = 1,
+        HEALER = 2,
+        DPS = 4,
+        MINION = 8,
+        BOSS = 16,
+        CREEP = 32,
+    }
+
+    public enum PrimaryAttribute
+    {
+        STR = 1,
+        AGI = 2,
+        INT = 3,
+        NON = 4,
+    }
+
+    public enum Type
+    {
+        INT,
+        INT256,
+        STRING,
+        FLOAT,
+        ENUM_CAREER,
+        ENUM_PRIMARY_ATTRIBUTE,
+    }
+
+    [Serializable]
+    public struct Field
+    {
+        public string Name;
+        public Type Type;
+        public string sValue;
+        public int iValue;
+        public float fValue;
+    }
+}
---
+++ EndlessFarmUnity/Assets/Scripts/Objects/UnitConfig.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using UnityEngine;
+
+namespace WE
+{
+    [CreateAssetMenu(fileName = "unit_config", menuName = "WE/Unit Config"), Serializable]
+    public class UnitConfig : ScriptableObject
+    {
+        public Field[] Fields;
+    }
+}
