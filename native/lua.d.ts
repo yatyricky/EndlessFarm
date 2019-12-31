c8ad99ba48c6fd014e6651328a49ab998887db83
Rick Sun <yatyricky@gmail.com>
2020-01-01 02:13:57 +0800

    1

--- native/lua.d.ts
+++ native/lua.d.ts
@@ -1,3 +1,9 @@
-declare function print(this: void, ...msg: any): void;
-declare function tostring(this: void, obj: any): string;
-declare function int256(this: void, id: string): number;
+/** @noSelfInFile **/
+
+declare function print(...msg: any): void;
+declare function tostring(obj: any): string;
+declare function int256(id: string): number;
+
+declare namespace console {
+    function log(...arg: any[]): void;
+}
